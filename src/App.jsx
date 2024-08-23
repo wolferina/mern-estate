e20a43211e90f4a692669a7b6f2c3c8f7b4082b5
wolferina <73065645+wolferina@users.noreply.github.com>
2024-08-24 01:42:05 +0330

    update app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <h1 className='text-red-500'>this is a test message</h1>
   )
 }
