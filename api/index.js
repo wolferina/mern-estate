f8782fcbbc8417c85acdd4e63d664e51e4094a3e
wolferina <73065645+wolferina@users.noreply.github.com>
2025-01-17 18:25:47 +0330

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = 3000;
+mongoose.connect(process.env.MANGO)
+  .then(() => {
+    console.log("connected to DB!")
+  })
+  .catch((err) => {
+    console.error(err);
+  })
 
 const app = express();
 
